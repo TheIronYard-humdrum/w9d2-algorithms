1e69b379747302a44927d2b5041b426ea6906e71
humdrum <jjrajani@gmail.com>
2016-08-02 17:23:39 -0400

    added form to input card number and validate

--- src/js/main.js
+++ src/js/main.js
@@ -1,69 +1,17 @@
-var visa = 4417123467899113;
+import $ from 'jquery'
 
-function consolidate(number) {
-  for ( var i = 0; i < number.length; i++ ) {
-    let digits = (""+number[i]).split("");
-    if (digits.length > 1) {
-      number[i] = sum(digits)
-    }
-  }
-  return number;
-}
+import { validate2 } from './validate'
 
-function double(number) {
-  number = number.map(function(num) {
-    return num + num;
-  })
-  return number;
-}
+var trueHTML = `<p>This card number is valid</p>`
+var falseHTML = `<p>This card number is invalid</p>`
 
-function sum(number) {
-  summed = number.reduce(function (a, b) {
-    return Number(a) + Number(b)
-  })
-  return summed
-}
 
-function evenIndexed(number) {
-  evens = [];
-  for (var i = 0; i < number.length; i += 2) {
-    evens.push(Number(number[i]))
-  }
-  return evens
-}
-
-function oddIndexed(number) {
-  odds = []
-  for (var i = 1; i <= number.length; i += 2) {
-    odds.push(Number(number[i]))
-  }
-  return odds
-}
-
-// add a simplifier to check if any of the summed
-// or doubled numbers resulted in double digit numbers
-
-function validate(cc) {
-  let digits = (""+cc).split(""); //split digits
-  odds = oddIndexed(digits); // get all odd indexes
-  evens = evenIndexed(digits); // get all even idexes
-  summedEvens = sum(evens); // sum evens
-  doubledOdds = double(odds); //dobule odds
-  doubledOddsConsolidated = consolidate(doubledOdds) //consolidate double digit numbers
-  summedOddsConsolidated = sum(doubledOddsConsolidated) //add sum the newly consolidated digits
-  let total = summedOddsConsolidated + summedEvens; // sum even and odd manipulated
-  if (total % 10 === 0) { // divisible by 10 valid check
-    return true
+$('form').on('submit', function (event) {
+  event.preventDefault();
+  let cc = $('input');
+  if ( validate2(cc[0].value) === true ) {
+    $('.container').append(trueHTML)
   } else {
-    return false
+    $('.container').append(falseHTML)
   }
-}
-
-function validate2(cc) {
-  let digits = (""+cc).split("");
-  odds = oddIndexed(digits);
-  evens = evenIndexed(digits);
-
-  total = sum(evens) + sum(consolidate(double(odds)))
-  return total % 10 === 0 ? true : false
-}
+})
---
+++ src/js/validate.js
@@ -0,0 +1,71 @@
+var visa = 4417123467899113;
+
+function consolidate(number) {
+  for ( var i = 0; i < number.length; i++ ) {
+    var digits = (""+number[i]).split("");
+    if (digits.length > 1) {
+      number[i] = sum(digits)
+    }
+  }
+  return number;
+}
+
+function double(number) {
+  var number = number.map(function(num) {
+    return num + num;
+  })
+  return number;
+}
+
+function sum(number) {
+  var summed = number.reduce(function (a, b) {
+    return Number(a) + Number(b)
+  })
+  return summed
+}
+
+function evenIndexed(number) {
+  var evens = [];
+  for (var i = 0; i < number.length; i += 2) {
+    evens.push(Number(number[i]))
+  }
+  return evens
+}
+
+function oddIndexed(number) {
+  var odds = []
+  for (var i = 1; i <= number.length; i += 2) {
+    odds.push(Number(number[i]))
+  }
+  return odds
+}
+
+// add a simplifier to check if any of the summed
+// or doubled numbers resulted in double digit numbers
+
+function validate(cc) {
+  var digits = (""+cc).split(""); //split digits
+  var odds = oddIndexed(digits); // get all odd indexes
+  var evens = evenIndexed(digits); // get all even idexes
+  var summedEvens = sum(evens); // sum evens
+  var doubledOdds = double(odds); //dobule odds
+  var doubledOddsConsolidated = consolidate(doubledOdds) //consolidate double digit numbers
+  var summedOddsConsolidated = sum(doubledOddsConsolidated) //add sum the newly consolidated digits
+  var total = summedOddsConsolidated + summedEvens; // sum even and odd manipulated
+  if (total % 10 === 0) { // divisible by 10 valid check
+    return true
+  } else {
+    return false
+  }
+}
+
+function validate2(cc) {
+  var digits = (""+cc).split("");
+  var odds = oddIndexed(digits);
+  var evens = evenIndexed(digits);
+
+  var total = sum(evens) + sum(consolidate(double(odds)))
+  return total % 10 === 0 ? true : false
+}
+
+export { validate2 }
